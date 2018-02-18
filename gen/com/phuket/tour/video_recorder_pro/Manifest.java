db31ca6890da6946a2b6e0dab6be55307ed90248
zhangbin <nowget@qq.com>
2018-02-18 22:26:53 +0800

    import as as project

---
+++ gen/com/phuket/tour/video_recorder_pro/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.phuket.tour.video_recorder_pro;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
